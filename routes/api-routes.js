114e18844ec0d45dc5efe4f118cc9339c3ea95d8
Elliott Wendel <ewendel88@gmail.com>
2017-11-17 16:28:49 -0600

    added database and working on put route,

---
+++ routes/api-routes.js
@@ -0,0 +1,52 @@
+// *********************************************************************************
+// api-routes.js - this file offers a set of routes for displaying and saving data to the db
+// *********************************************************************************
+
+// Dependencies
+// =============================================================
+var User = require("../models/user.js");
+
+
+// Routes
+// =============================================================
+module.exports = function(app) {
+
+  // Get all chirps
+  app.get("/api/all", function(req, res) {
+
+    // Finding all Chirps, and then returning them to the user as JSON.
+    // Sequelize queries are aynchronous, which helps with percieved speed.
+    // If we want something to be guaranteed to happen after the query, we'll use
+    // the .then function
+    User.findAll({}).then(function(results) {
+      // results are available to us inside the .then
+      res.json(results);
+    });
+
+  });
+
+  // Add a chirp
+  app.post("/api/new", function(req, res) {
+
+    console.log("User Data:");
+    console.log(req.body);
+
+    User.create({
+      name: "Elliott",
+      gender: 1,
+      age: 29,
+      ageRangeHigh: 35,
+      ageRangeLow: 21,
+      phoneNumber: "555-555-5555"
+      sexualPref: 2,
+      language: "English",
+      interests: "cars boats planes submarines guns freedom beef fire jeeps monkeys"
+      created_at: req.body.created_at
+    }).then(function(results) {
+      // `results` here would be the newly created chirp
+      res.end();
+    });
+
+  });
+
+};
