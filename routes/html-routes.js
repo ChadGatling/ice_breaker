f58c48dfba3d414ea4e02839fba7ac0621ef932e
Lisa Jetton <ldjetton21@gmail.com>
2017-11-18 11:18:49 -0600

    upates to map on meet file and css changes to html files

---
+++ routes/html-routes.js
@@ -0,0 +1,19 @@
+var path = require("path");
+
+module.exports = function(app) {
+	app.get("/signup", function(req.res) {
+		res.sendFile(path.join(__dirname, "../public/html/sign-up.html"))
+	});
+
+	app.get("/profile", function(req.res) {
+		res.sendFile(path.join(__dirname, "../public/html/profile.html"))
+	});
+
+	app.get("/meet", function(req.res) {
+		res.sendFile(path.join(__dirname, "../public/html/meet.html"))
+	});
+
+	app.use(function(req, res) {
+		res.sendFile(path.join(__dirname, "../public/html/welcome.html"))
+	});
+};
---
+++ server.js
@@ -0,0 +1,48 @@
+// Server.js - This file is the initial starting point for the Node/Express server.
+
+
+//Dependencies (General)
+// =============================================================
+var express = require("express");
+var bodyParser = require("body-parser");
+
+
+//Map Dependencies
+// ===============================================
+var LeafletFactory = require("leaflet");
+var components = require("server-components");
+
+// Express-App Setup 
+// =============================================================
+var app = express();
+var PORT = process.env.PORT || 8080;
+
+// Requires "db" from our models folder (insie index.js) to for syncing purposes
+// ================================================================================
+var db = require("./models");
+
+//Data parsing by Express (Set-up)
+// ======================================
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.text());
+app.use(bodyParser.json({ type: "application/vnd.api+json" }));
+
+// Static Directory Reference
+// ======================================
+app.use(express.static("public"));
+
+// Routes
+// =============================================================
+require("./routes/html-routes.js")(app);
+require("./routes/profile-api-routes.js")(app);
+require("./routes/user-api-routes.js")(app);
+require("./routes/meet-api-routes.js")(app);
+
+// Syncing our sequelize models and then starting our Express app
+// =============================================================
+db.sequelize.sync({ force: true }).then(function() { //FORCING Sequelize to automatically sync!!
+  app.listen(PORT, function() {
+    console.log("App listening on PORT " + PORT);
+  });
+});
