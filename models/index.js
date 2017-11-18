13dbd95a6bb6ef3dc8ddb4bd2da27bf1e6c8ae11
Lisa Jetton <30459942+JettTech@users.noreply.github.com>
2017-11-18 12:58:26 -0600

    Update index.js

--- models/index.js
+++ models/index.js
@@ -1,7 +1,3 @@
-//Models - models in Database (reference to the DB Tables),
-//Views - views by public (reference to the html pages/ Front-end Content),
-//Controller - routues-contoller (refers to the )
-
 'use strict';
 
 
@@ -14,12 +10,10 @@ var Sequelize = require('sequelize');
 var basename  = path.basename(module.filename);
 var env       = process.env.NODE_ENV || 'development';
 var config    = require(__dirname + '/../config/config.json')[env];
-var db        = {}; //cfeate empty db object
+var db        = {};
 
 
 // DATABASE SELECTION  -->> Locates DB Source, 
-    //depending on whether the (REMOTE) process enviornment exitst, 
-        //and if not, defauls the DB Connection to the local.
 //======================================================================================================
 if (config.use_env_variable) {
   var sequelize = new Sequelize(process.env[config.use_env_variable]);
@@ -30,23 +24,23 @@ if (config.use_env_variable) {
 // File-System Process
 //========================================================================================================
 fs
-  .readdirSync(__dirname) //this reads and syncs to the directory name selected (if not mentioned specifically, default === current directory)
-  .filter(function(file) {
-    return (file.indexOf('.') !== 0) && (file !== basename) && (file.slice(-3) === '.js'); //FS (FileSystem) Method that filters through files and locates the js files..
+  .readdirSync(__dirname)
+   .filter(function(file) {
+    return (file.indexOf('.') !== 0) && (file !== basename) && (file.slice(-3) === '.js'); //FS filters through files and locates the js files..
   })
   .forEach(function(file) {
-    var model = sequelize['import'](path.join(__dirname, file)); //FS (FileSystem) Method that imports the js files (the model files) in the Config Directory...
-    db[model.name] = model; //this establishes a KEY into the above db OBJECT, this key === the imported imported js file (the model files) in Config...
-  });
+    var model = sequelize['import'](path.join(__dirname, file)); //FS imports the js files (the model files) in the Config Directory...
+    db[model.name] = model; 
+});
 
 Object.keys(db).forEach(function(modelName) {
-  if (db[modelName].associate) {// IF TRUTHY, IF the model name HAS an associate property, then...
-    db[modelName].associate(db); //..review this further!!!
+  if (db[modelName].associate) {
+    db[modelName].associate(db);
   }
 });
 
 
-// FILLS AND EXPORTS THE "db", by setting the parameters of object "db" to the sequelize db environment and sequelize tables/model files(includes all updates/revisions/changes..)
+// FILLS AND EXPORTS THE "db"
 //=====================================================================================================================
 db.sequelize = sequelize; //this references the DB Environment
 db.Sequelize = Sequelize; // this references the Sequelize npm packages and thus all the methods and use cases in the js files..
