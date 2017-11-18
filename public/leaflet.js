4f9e868542616de2f3b2297c0800e8738dee6a48
Lisa Jetton <ldjetton21@gmail.com>
2017-11-18 15:10:17 -0600

    updated files'

--- public/leaflet.js
+++
@@ -1,6 +0,0 @@
-module.exports = function (window, document) {
-  var navigator = window.navigator;
-    console.log(navigator);
-    //[...insert main Leaflet code...]
-  return window.L.noConflict();
-};
