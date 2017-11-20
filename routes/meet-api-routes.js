c08c42eabb12b1459bd8064ee20b10f194162250
Lisa Jetton <ldjetton21@gmail.com>
2017-11-20 14:42:19 -0600

    removed map functionality - will change out for firebase/google compatible api

--- routes/meet-api-routes.js
+++ routes/meet-api-routes.js
@@ -1,19 +1,2 @@
 // Requiring our models
 var db = require("../models");
-
-// MAP Dependencies  & Functions..
-// =============================================================
-var LeafletFactory = require("leaflet");
-var components = require("server-components");
-
-var MapElement = components.newElement();   
-
-MapElement.createdCallback = function (document) {
-  var L = LeafletFactory(new components.dom.Window(), document);
-  var map = L.map(this).setView([41.3851, 2.1734], 12);
-  L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    maxZoom: 19,
-  }).addTo(map);
-};
-
-components.registerElement("leaflet-map", {prototype: MapElement});
